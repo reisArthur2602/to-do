36f761631142d54fb768b49de75bb4000a1e6c06
arthur <contato.arthurreis222@gmail.com>
2024-02-01 16:51:23 -0300

    limpando loca lstorage ao fazer logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ import LogoImg from "../../assets/logo.svg";
 import { TbLogout } from "react-icons/tb";
 import { signOut } from "firebase/auth";
 import { auth } from "../../services/firebaseConnection";
+import { useEffect, useState } from "react";
 
 const HeaderContainer = styled.header`
   width: 100%;
@@ -19,14 +20,16 @@ const ButtonLogout = styled.button`
   cursor: pointer;
 `;
 
-const handleLogout = () => {
-  signOut(auth);
-};
-
 const Header = () => {
+  const handleLogout = () => {
+    signOut(auth);
+    localStorage.clear();
+  };
+
   return (
     <HeaderContainer>
       <img src={LogoImg} alt={LogoImg} />
+
       <ButtonLogout onClick={handleLogout}>
         <TbLogout size={29} color="#864AF9" />
       </ButtonLogout>
